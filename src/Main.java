9e904cfb7b162d1133dd3782ad52c2e570d698e2
Jean Tu <tujean27@gmail.com>
2020-09-02 19:57:04 -0700

    COMPLETED 9/2/2020

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,18 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
+        char studentFirstInitial = 'J';
+        char studentLastInitial = 'T';
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "Jean";
+        String studentLastName = "Tu";
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
     }
 }
