aad5d304287aca68ebc99e837d2c5b5e702b927b
Jean Tu <tujean27@gmail.com>
2020-09-02 20:20:11 -0700

    COMPLETED 9/2/2020 - If/else statements and how the flow has to be entered within the curly brackets

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+// Learning IF Else statements
+// They should be enclosed in the curly brackets
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,5 +10,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int inputtedNum = scanner.nextInt();
+        if (inputtedNum < 5){
+            System.out.println("Enjoy the good luck a friend brings you");
+        } else {
+            System.out.println("your shoe selection will make you happy today");
+        }
+
     }
 }
