31ef9d9eb5125e3f9a4aa93e0a81ac932c0c2873
Jean Tu <tujean27@gmail.com>
2020-09-02 19:55:11 -0700

    UPDATE: COMPLETED

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char StudentFirstInitial = 'J';
+        char StudentLastInitial = 'T';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(StudentFirstInitial);
+        System.out.println(StudentLastInitial);
+        System.out.println(hasPerfectAttendance);
     }
 }
