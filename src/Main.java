0964c5d81034d26740c1c3b2e7cdbf69cfd265b4
Jean Tu <tujean27@gmail.com>
2020-09-03 20:43:12 -0700

    COMPLETED 9/3/2020 - function arguments

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void calculateTotalMealPrice() {
+    public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxrate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxrate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        System.out.println("Your total meal price is " + result);
     }
 
     public static void main(String[] args) {
+        calculateTotalMealPrice(30, .2, .08 );
     }
 
 }
