3e82a3bac76a2f3af18dbe40a86e455459b41e65
Jean Tu <tujean27@gmail.com>
2020-09-03 20:47:07 -0700

    COMPLETED 9/3/2020 - returning data from functions

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
-
-    public static void calculateTotalMealPrice(double listedMealPrice,
+    // it's not void, but double in this case b/c we're returning a double from this function
+    public static double calculateTotalMealPrice(double listedMealPrice,
                                                double tipRate,
                                                double taxRate) {
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
         double result = listedMealPrice + tip + tax;
-        System.out.println("Your total meal price is " + result);
+        return result;
     }
 
     public static void main(String[] args) {
-        
+        double groupTotalMealPrice = calculateTotalMealPrice(100, .2, 0.08);
+        System.out.println(groupTotalMealPrice);
+
+        double individalMealprice = groupTotalMealPrice/5;
+        System.out.println(individalMealprice);
     }
 
 }
