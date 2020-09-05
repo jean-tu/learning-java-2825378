d8e08292ca705c77a28e1adfb230ab17ecc4c33c
Jean Tu <tujean27@gmail.com>
2020-09-05 13:37:50 -0700

    COMPLETE 9/5/2020 - challenge

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
+    public static double salaryCalculator(double hoursPerWeek, double amountPerHour){
+        double weeklyPaycheck = hoursPerWeek * amountPerHour;
+        return weeklyPaycheck * 52;
+    }
     public static void main(String[] args) {
-        
+        double salary = salaryCalculator(40, 50);
+        System.out.println(salary);
     }
 
 }
