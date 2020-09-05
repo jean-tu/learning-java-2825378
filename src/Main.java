7f46daa32b6255c5d159bd181cbb67fc6ebb3617
Jean Tu <tujean27@gmail.com>
2020-09-05 13:40:06 -0700

    UPDATE 9/5/2020 - additional items with vacataion days

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static double salaryCalculator(double hoursPerWeek, double amountPerHour){
+    public static double salaryCalculator(double hoursPerWeek, double amountPerHour, int vacationDays){
         if (hoursPerWeek <0){
             return -1;
         }
@@ -8,10 +8,11 @@ public class Main {
             return -1;
         }
         double weeklyPaycheck = hoursPerWeek * amountPerHour;
-        return weeklyPaycheck * 52;
+        double unpaidTime = vacationDays * amountPerHour *8;
+        return (weeklyPaycheck * 52)-unpaidTime;
     }
     public static void main(String[] args) {
-        double salary = salaryCalculator(40, 50);
+        double salary = salaryCalculator(40, 50,8);
         System.out.println(salary);
     }
 
