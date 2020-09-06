3764252620e9af4e932517c321166ed18a77d924
Jean Tu <tujean27@gmail.com>
2020-09-06 13:28:43 -0700

    COMPLETE 9/6/2020

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main {
         System.out.println(triangleAArea);
 
         // Triangle.findArea()? --> Math.pow(2,3)?
-
+        double triangleBArea = triangleB.findArea();
+        System.out.println(triangleBArea);
         
     }
 
